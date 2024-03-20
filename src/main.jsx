6613643523e662b67bacb3471064df092f042d52
SamiaAkter21 <aktersamia1220@gmail.com>
2024-03-20 21:55:26 +0600

    added BrowseRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowseRouter basename={"/Portfolio/"}>
+      <App />
+    </BrowseRouter>
   </React.StrictMode>,
 )
